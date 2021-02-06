dce160d72a09f0a254a384c25a164bb9681997ec
Daulen Zhangabylov <daulenz@gmail.com>
2021-02-06 17:18:43 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -23,7 +23,12 @@ def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
 
-
+literal_aoh_array=[
+    DON_G,
+    JOELLE_VD,
+    PAT_M,
+    KATE_G,
+    BRUCE_G]
 
 
 end
